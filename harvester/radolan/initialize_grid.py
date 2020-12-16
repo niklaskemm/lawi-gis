29bd2c10755a5b5ada2e8013e3dc294a58e8a1dd
Niklas Kemm <niklas.kemm@googlemail.com>
2020-12-16 11:20:33 +0100

    Update: radolan_grid transform and upload to db

--- harvester/radolan/initialize_grid.py
+++ harvester/radolan/initialize_grid.py
@@ -1,14 +1,39 @@
 import os
 import psycopg2
-import wradlib as wrl
 import psycopg2.extras
+import geopandas as gpd
 
 from tqdm import tqdm
 from setup import setup_env
+from shapely.wkt import dumps
 from datetime import datetime
 
 
-def export_data():
+def create_values():
+    setup_env()
+
+    radolan_grid_path = os.getenv("RADOLAN_GRID_PATH")
+    values = []
+
+    print("Creating dataframe...")
+    radolan_grid = gpd.read_file(radolan_grid_path).to_crs("epsg:4326")
+    print("Dataframe created.")
+
+    print("Creating values array...")
+    for _, row in tqdm(radolan_grid.iterrows(), leave=False, total=len(radolan_grid), unit=" rows"):
+        time = datetime.strptime(datetime.strftime(
+            datetime.now(), "%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
+        geom = dumps(row.geometry, rounding_precision=5)
+        values.append([
+            geom, geom, time, time
+        ])
+
+    print("Values array created and exported.")
+
+    return values
+
+
+def upload_data(values):
     setup_env()
 
     db_server = os.getenv("DB_SERVER")
@@ -16,42 +41,21 @@ def export_data():
     db_username = os.getenv("DB_USERNAME")
     db_password = os.getenv("DB_PASSWORD")
     db_database = os.getenv("DB_DATABASE")
-    # db_datatable = os.getenv("DB_DATATABLE")
 
     config = f"host='{db_server}' port={db_port} user='{db_username}' password='{db_password}' dbname='{db_database}'"
 
-    radolan_grid_wgs84 = wrl.georef.get_radolan_grid(900, 900, wgs84=True)
-
-    radolan_grid_wgs84_wkt = []
-
-    for x in tqdm(radolan_grid_wgs84.tolist(), total=900):
-        for y in x:
-            lat = y[0]
-            lon = y[1]
-            wkt_representation = f"POINT ({lat} {lon})"
-            radolan_grid_wgs84_wkt.append(wkt_representation)
-
-    time = datetime.strptime(datetime.strftime(
-        datetime.now(), "%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
-    values = []
-
-    for x in tqdm(radolan_grid_wgs84_wkt, total=810000):
-        values.append(
-            [
-                x, time, time
-            ]
-        )
-
+    print("Uploading data...")
     with psycopg2.connect(config) as conn:
         with conn.cursor() as cur:
-            # cur.execute("CREATE TABLE IF NOT EXISTS gridgeoms")
-            # cur.execute("DELETE FROM gridgeoms")
             psycopg2.extras.execute_batch(
                 cur,
-                'INSERT INTO gridgeoms (geom, "createdAt", "updatedAt") VALUES (ST_GeomFromText(%s, 4326), %s, %s);',
+                'INSERT INTO gridgeoms (geom, centroid, "createdAt", "updatedAt") VALUES (ST_GeomFromText(%s, 4326), ST_Centroid(ST_GeomFromText(%s, 4326)), %s, %s);',
                 values
             )
+    print("Data uploaded.")
 
 
 if __name__ == "__main__":
-    export_data()
+    upload_data(
+        create_values()
+    )
