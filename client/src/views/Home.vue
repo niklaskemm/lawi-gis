cf1a5511dc2265e977dff970d67610af596cf5cf
Niklas Kemm <niklas.kemm@googlemail.com>
2021-01-12 17:02:31 +0100

    Update: es-lint...

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Home"
-})
+});
 </script>
 
 <style lang="scss" scoped></style>
