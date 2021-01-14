eeb6709e2de021bd1b796be029caedd2f724870e
Niklas Kemm <niklas.kemm@googlemail.com>
2021-01-14 15:27:33 +0100

    Add: FieldController with postField functionality

---
+++ server/utils/controllers/FieldController.js
@@ -0,0 +1,27 @@
+const db = require("../../database/models/index")
+const Field = require("../../database/models").field
+
+module.exports = {
+  async index(req, res) {
+    try {
+      // const grid = await gridgeom.findAll({})
+      // res.send(grid)
+      res.send("Nope. Too much data.")
+    } catch (err) {
+      res.status(500).send({ error: err })
+    }
+  },
+
+  async postField(req, res) {
+    try {
+      const field = Field.create(req.body)
+      res.send(field)
+    } catch (err) {
+      if ((err.name = "SequelizeDatabaseError")) {
+        res.status(400).send(`Invalid input syntax: ${req.body}`)
+      } else {
+        res.status(503).send({ error: err })
+      }
+    }
+  }
+}
