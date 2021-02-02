6a84e1f3c1416bc8b5c3650fc68077aa047a30d6
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-02 14:18:18 +0100

    Update: delete testmapview, add general testing view

---
+++ client/src/components/Testing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1> Hi World! </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue';
+import {
+  getRadolanDataByFieldId
+} from "../utils/functions/getRadolanDataByFieldId";
+
+export default defineComponent({
+  name: "Testing",
+
+  setup() {
+    onMounted(async () => {
+      const fieldId = "94601639-7055-4bcc-abfe-90b8c1dc6cf9"
+      const startDate = ""
+      const endDate = ""
+      const result = await getRadolanDataByFieldId(fieldId, startDate, endDate)
+    })
+  }
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ client/src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <testing />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Testing from "../components/Testing.vue";
+
+export default defineComponent({
+  name: "TestView",
+  components: {
+    Testing
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
