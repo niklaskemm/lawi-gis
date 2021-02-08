89bc84298f52a253f7c34f6d7fa98badef23ed4e
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-08 15:19:46 +0100

    Add: reusable chart component

---
+++ client/src/components/chart.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <canvas ref="chart"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import { Chart } from "chart.js";
+
+export default defineComponent({
+  name: "MyChart",
+  props: {
+    labels: {
+      type: Array as any,
+      required: true
+    },
+    data: {
+      type: Array as any,
+      required: true
+    }
+  },
+
+  setup(props) {
+    const chart = ref(null)
+
+    onMounted(() => {
+      const myChart = new Chart(chart.value!, {
+        type: "bar",
+        data: {
+          labels: props.labels,
+          datasets: [
+            {
+              type: "line",
+              data: props.data,
+              borderColor: "blue",
+              fill: false
+            },
+            {
+              type: "bar",
+              data: props.data,
+              backgroundColor: "lightblue"
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: "Niederschlag in mm"
+          },
+          legend: { display: false }
+        }
+      });
+    })
+    return { chart };
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
