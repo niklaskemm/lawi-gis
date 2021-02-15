32a3dc23bc7222270ad41d1dc6ae49a221affa28
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-15 10:18:52 +0100

    Update: improve static map capabilities

--- client/src/components/StaticMap.vue
+++ client/src/components/StaticMap.vue
@@ -1,205 +1,111 @@
 <template>
-  <div>
-    <div id="map" class="map"></div>
-    <div id="popupContainer" class="popup-container" ref="popupContainer">
-      <!-- TODO add closePopup function -->
-      <a href="#" id="popupCloser" class="popup-closer" />
-      <div id="popupContent" class="popup-content">
-        <h3>Grid ID: {{ gridId }}</h3>
-        <h2>Niederschlag der letzten 14 Tage:</h2>
-        <h2>{{ radolanValue14 }}mm</h2>
-      </div>
-      <router-link :to="{ name: 'Grid', params: { gridId: gridId } }">Mehr anzeigen</router-link>
-    </div>
-  </div>
+  <div id="map" class="map"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 
 import "ol/ol.css";
+import { Map, View } from "ol";
 import GeoJSON from "ol/format/GeoJSON";
-import { Map, Overlay, View } from "ol";
-import { fromLonLat, transform } from "ol/proj";
-import { Vector as VectorSource } from "ol/source";
+import { Attribution } from "ol/control";
+import { Tile as TileLayer } from "ol/layer";
 import { Vector as VectorLayer } from "ol/layer";
-import { Draw, Modify, Snap, defaults as defaultInteractions } from "ol/interaction";
-import GeometryType from "ol/geom/GeometryType";
-import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
-import { Attribution, defaults as defaultControls } from "ol/control";
+import { OSM, BingMaps, Vector as VectorSource } from "ol/source";
 
-import { targetProjection } from "../utils/variables";
-import { getGridDataAtLocation } from "../utils/functions/getGridDataAtLocation";
-import { getRadolanDataAtLocation } from "../utils/functions/getRadolanDataAtLocation";
+import { BingMapsApiKey } from "../../src/api_key_config.js";
+import { getGridById } from "../utils/functions/getGridById";
+import { getFieldById } from "../utils/functions/getFieldById";
 
 export default defineComponent({
-  name: "Map",
+  name: "StaticMap",
   props: {
-    baseLayers: {
-      // TODO get rid of 'any'... PropType?
-      // eslint-disable-next-line
-      type: Array as any,
-      required: true
-    },
-    centerLon: {
-      type: Number,
-      default: 0
-    },
-    centerLat: {
-      type: Number,
-      default: 0
-    },
-    zoom: {
-      type: Number,
-      default: 10
-    },
-    clickable: {
+    fieldId: {
       type: Boolean,
-      default: true
+      default: false
+    },
+    gridId: {
+      type: Boolean,
+      default: false
+    },
+    id: {
+      type: String,
+      required: true
     }
   },
 
   setup(props) {
-    const attribution = new Attribution({
-      collapsible: true
-    });
+    onMounted(async () => {
+      let response: any
 
-    const view = new View({
-      center: fromLonLat([props.centerLon, props.centerLat]),
-      zoom: props.zoom
-    });
-
-    const popupContainer = ref(null);
-    const overlay = new Overlay({
-      autoPan: {
-        animation: {
-          duration: 250
-        },
-        margin: 30
+      if (props.fieldId) {
+        response = await getFieldById(props.id)
+      } else if (props.gridId) {
+        response = await getGridById(parseInt(props.id))
+      } else {
+        console.log("ERROR!")
       }
-    });
 
-    const layerGrid = new VectorLayer({
-      updateWhileInteracting: true
-    });
+      const featureObject = new GeoJSON().readFeature(
+        response.response.data.geom, { featureProjection: "EPSG:3857" }
+      )
 
-    const sourceDraw = new VectorSource();
-    const layerDraw = new VectorLayer({
-      source: sourceDraw,
-      style: new Style({
-        fill: new Fill({
-          color: "rgba(255, 255, 255, 0.2)"
-        }),
-        stroke: new Stroke({
-          color: "#ffcc33",
-          width: 2
-        }),
-        image: new CircleStyle({
-          radius: 7,
-          fill: new Fill({
-            color: "#ffcc33"
-          })
-        })
+      const attribution = new Attribution({
+        collapsible: true
+      });
+
+      const sourceObject = new VectorSource({
+        features: [featureObject]
       })
-    });
 
-    const layers = [] as any;
+      const layerObject = new VectorLayer({
+        source: sourceObject
+      });
 
-    for (const baselayer of props.baseLayers) {
-      layers.push(baselayer);
-    }
+      const layerBing = new TileLayer({
+        source: new BingMaps({
+          key: BingMapsApiKey,
+          imagerySet: "AerialWithLabelsOnDemand"
+        })
+      });
 
-    layers.push(layerDraw);
+      const layerOSM = new TileLayer({
+        source: new OSM()
+      });
 
-    const modify = new Modify({ source: sourceDraw });
-
-    let draw, snap;
-
-    const gridId = ref(0);
-    const radolanValue14 = ref(0);
-
-    const interactions = defaultInteractions({
-      doubleClickZoom: false,
-      mouseWheelZoom: false
-    });
-
-    const mapOptions = {
-      target: "map",
-      layers: layers,
-      overlay: [],
-      view: view,
-      controls: defaultControls({ attribution: false }).extend([attribution]),
-      interactions: interactions
-    };
-
-    onMounted(() => {
-      const map = new Map(mapOptions);
-      // eslint-disable-next-line
-      overlay.setElement(popupContainer.value!);
-      map.addInteraction(modify);
-
-      function addInteractions() {
-        draw = new Draw({
-          source: sourceDraw,
-          type: GeometryType.POLYGON
-        });
-        map.addInteraction(draw);
-        snap = new Snap({ source: sourceDraw });
-        map.addInteraction(snap);
+      let layers = [] as any
+      if (props.fieldId) {
+        layers = [layerBing, layerObject]
+      } else if (props.gridId) {
+        layers = [layerOSM, layerObject]
+      } else {
+        console.log("ERROR!")
       }
 
-      addInteractions();
+      const view = new View({
+        center: [10, 10],
+        zoom: 10
+      });
 
-      if (props.clickable) {
-        map.on("singleclick", async function (event) {
-          const coordinate = event.coordinate;
+      const mapOptions = {
+        target: "map",
+        layers: layers,
+        view: view,
+        controls: []
+      };
 
-          const APIResultGrid = await getGridDataAtLocation(
-            transform(coordinate, "EPSG:3857", targetProjection)
-          );
+      const map = new Map(mapOptions)
 
-          const APIResultRadolan = await getRadolanDataAtLocation(
-            transform(coordinate, "EPSG:3857", targetProjection)
-          );
-
-          gridId.value = APIResultGrid.Data.gridId;
-          radolanValue14.value = APIResultRadolan.Data.radolanValue14;
-
-          try {
-            map.removeLayer(layerGrid);
-          } finally {
-            const gridSource = new VectorSource({});
-
-            const gridFeature = new GeoJSON().readFeature(
-              APIResultGrid.Data.gridExtentGeoJSON,
-              {
-                featureProjection: "EPSG:3857"
-              }
-            );
-
-            gridSource.addFeature(gridFeature);
-
-            layerGrid.unset("Source");
-            layerGrid.setSource(gridSource);
-
-            overlay.setPosition(
-              fromLonLat(APIResultGrid.Data.gridCentroidLonLat)
-            );
-
-            // view.animate({
-            //   center: coordinate,
-            //   zoom: 13.65,
-            //   duration: 500
-            // });
-
-            map.addLayer(layerGrid);
-            map.addOverlay(overlay);
-          }
-        });
+      const feature = sourceObject.getFeatures()[0]
+      if (feature) {
+        const extent = feature!.getGeometry()!.getExtent()
+        view.fit(extent, {
+          size: map.getSize(),
+          padding: [50, 25, 50, 25],
+          maxZoom: 18
+        })
       }
     });
-
-    return { popupContainer, gridId, radolanValue14 };
   }
 });
 </script>
@@ -209,49 +115,5 @@ export default defineComponent({
   position: absolute;
   width: 100%;
   height: 100%;
-}
-
-.popup-container {
-  position: absolute;
-  background-color: white;
-  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
-  padding: 15px;
-  border-radius: 10px;
-  border: 1px solid #cccccc;
-  bottom: 12px;
-  left: -50px;
-  min-width: 280px;
-}
-
-.popup-container:after,
-.popup-container:before {
-  top: 100%;
-  border: solid transparent;
-  content: " ";
-  height: 0;
-  width: 0;
-  position: absolute;
-  pointer-events: none;
-}
-.popup-container:after {
-  border-top-color: white;
-  border-width: 10px;
-  left: 48px;
-  margin-left: -10px;
-}
-.popup-container:before {
-  border-top-color: #cccccc;
-  border-width: 11px;
-  left: 48px;
-  margin-left: -11px;
-}
-.popup-closer {
-  text-decoration: none;
-  position: absolute;
-  top: 2px;
-  right: 8px;
-}
-.popup-closer:after {
-  content: "✖";
 }
 </style>
