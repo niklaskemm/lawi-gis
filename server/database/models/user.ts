503e6d95c12079da43a73755058a01f4929035f6
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-16 09:10:57 +0100

    Fix: message upon validation fail

--- server/database/models/user.ts
+++ server/database/models/user.ts
@@ -1,48 +1,53 @@
 const bcrypt = require("bcryptjs")
 
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('user', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-        msg: "Please provide a valid email adress."
+  var User = sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: { args: true, msg: "Please provide a valid email address." }
+        }
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [6, 20],
+            msg:
+              "The length of the password must not be shorter than 6 characters and must not exceed 20 characters."
+          }
+        }
       }
     },
-    firstname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [6,16],
-        msg: "Password needs to be at atleast 6 characters in length and 16 at most."
+    {
+      hooks: {
+        afterValidate: function (user) {
+          user.password = bcrypt.hashSync(user.password, 8)
+        }
       }
     }
-  }, {
-    hooks: {
-      afterValidate: function (user) {
-        user.password = bcrypt.hashSync(user.password, 8)
-      }
-    }
-  })
+  )
 
   User.associate = (models) => {
-    User.belongsToMany(models.field, { through:"user_field" }
-    )
+    User.belongsToMany(models.field, { through: "user_field" })
   }
   return User
 }
