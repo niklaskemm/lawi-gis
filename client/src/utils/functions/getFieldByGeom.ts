9c6bfc21ef5eee6627001509eacdb0bd121d3593
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:19:14 +0100

    Remove: console.log()

--- client/src/utils/functions/getFieldByGeom.ts
+++ client/src/utils/functions/getFieldByGeom.ts
@@ -3,7 +3,6 @@ import { stringify } from "wkt"
 
 export async function getFieldByGeom(geom) {
   const geomWKT = stringify(geom)
-  console.log(geomWKT)
   const response = await FieldService.getFieldByGeom(geomWKT)
 
   return { response }
