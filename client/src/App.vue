4860b816da839b72194df4c6443955540f4ce1a9
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:24:33 +0100

    Update: include navbar app-wide

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
+    <navbar />
     <Suspense>
       <template #default>
-        <router-view />
+        <router-view class="body" />
       </template>
       <template #fallback>
         <span>I'm a loading screen, I'm waiting the view to be ready!</span>
       </template>
     </Suspense>
-    <!-- <navbar /> -->
   </div>
 </template>
 
 <script lang="ts">
-// import Navbar from "@/components/Navbar.vue";
+import Navbar from "@/components/Navbar.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "app",
   components: {
-    // Navbar
+    Navbar
   }
 });
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
