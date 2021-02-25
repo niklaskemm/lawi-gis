0bd84ede4734804122c13d8de99184093c9ea108
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:24:13 +0100

    Update: inlcude dynamic login/register/profile in navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -5,27 +5,63 @@
         <router-link class="navbar__link" to="/">Home</router-link>
       </li>
       <li class="navbar__item">
-        <router-link class="navbar__link" to="/about">About</router-link>
+        <router-link class="navbar__link" to="/explore">Explore</router-link>
       </li>
       <li class="navbar__item">
-        <router-link class="navbar__link" to="/testing">Testing</router-link>
+        <router-link class="navbar__link" to="/about">About</router-link>
+      </li>
+      <li v-if="!store.getters.getToken" class="navbar__item">
+        <router-link class="navbar__link" to="/login">Login</router-link>
+      </li>
+      <li v-if="!store.getters.getToken" class="navbar__item">
+        <router-link class="navbar__link" to="/register">Register</router-link>
+      </li>
+      <li v-if="store.getters.getToken" class="navbar__item">
+        <router-link class="navbar__link" :to="{path: `/user/${user.id}`}">{{user.firstname}}</router-link>
+      </li>
+      <li v-if="store.getters.getToken" class="navbar__item">
+        <router-link @click="logout" class="navbar__link" to="/">Logout</router-link>
       </li>
     </ul>
   </nav>
 </template>
 
-<script>
-import { defineComponent } from "vue"
+<script lang="ts">
+import {
+  defineComponent,
+  computed,
+  ref,
+  onUpdated,
+  watchEffect
+} from "vue"
+import { useStore } from "vuex"
+import { useRouter } from "vue-router"
 
 export default defineComponent({
   name: "Navbar",
+  components: {},
+
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+
+    function logout() {
+      store.commit("setUser", null)
+      store.commit("setToken", null)
+    }
+
+    const user = computed(() => store.getters.getUser)
+
+    return { store, router, logout, user }
+  }
 })
 </script>
 
 <style lang="scss" scoped>
 .navbar {
   position: fixed;
-  border: solid 1px $primary;
+  z-index: 10;
+  background: white;
   bottom: 0;
   left: 0;
   width: 100%;
@@ -40,11 +76,19 @@ export default defineComponent({
       list-style: none;
 
       & .navbar__link {
-        font-weight: bold;
+        font-weight: lighter;
         color: #2c3e50;
         text-decoration: none;
       }
     }
+  }
+}
+
+@media (min-width: 1000px) {
+  .navbar {
+    position: sticky;
+    bottom: unset;
+    top: 0;
   }
 }
 </style>
