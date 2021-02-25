5366ee1012d602156a2f40aba248f6dea9d95a80
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:22:36 +0100

    Update: include button to change data timeframe

--- client/src/views/field/_id/index.vue
+++ client/src/views/field/_id/index.vue
@@ -9,14 +9,22 @@
     </div>
     <div class="body">
       <h1>{{ fieldName }}</h1>
-      <h2>Niederschlag der letzten {{ numberOfDays}} Tage: {{ complete }}mm</h2>
+      <br />
+      <input type="date" name="eventDateString" v-model="eventDateString">
+      <br />
+      <button @click=printDate()>Print Date</button>
+      <br />
+      <h2>Gesamtniederschlag zwischen</h2>
+      <h2>{{ startDatePrint }} und {{ endDatePrint }}:</h2>
+      <h2>{{ complete }}mm</h2>
+
       <div class="test">
         <div class="chart-container">
-          <my-chart :labels=timestampsDailyChart :data=radolanDailyValuesArray />
+          <my-chart :labels='timestampsDailyChart' :data='radolanDailyValuesArray' :key="chartKey" />
         </div>
         <p />
         <div class="table-container">
-          <my-table :cols=timestampsDailyTable :data=radolanHourlyValuesArray />
+          <my-table :cols='timestampsDailyTable' :data='radolanHourlyValuesArray' :key="tableKey" />
         </div>
       </div>
     </div>
@@ -51,19 +59,57 @@ export default defineComponent({
     const route = useRoute();
     const fieldId = route.params.fieldId.toString();
 
+    const eventDateString = ref("")
+    const chartKey = ref(0)
+    const tableKey = ref(0)
+
     const field = await getFieldById(fieldId)
     const fieldName = ref(field.response.data.name)
 
     const numberOfDays = ref(14);
-    const { startDateString, endDateString } = createStartEndDateString(numberOfDays.value)
+    const startDateString = ref(createStartEndDateString(numberOfDays.value).startDateString)
+    const endDateString = ref(createStartEndDateString(numberOfDays.value).endDateString)
 
-    const response = await getRadolanDataByFieldId(fieldId, startDateString, endDateString)
+
+    const datetimeOptions = {
+      // weekday: 'long',
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    }
+
+    const startDatePrint = ref(new Date(startDateString.value).toLocaleDateString("de-DE", datetimeOptions))
+    const endDatePrint = ref(new Date(endDateString.value).toLocaleDateString("de-DE", datetimeOptions))
+
+    const response = await getRadolanDataByFieldId(fieldId, startDateString.value, endDateString.value)
 
     const radolanDailyValuesArray = ref(response.radolanDaily)
     const radolanHourlyValuesArray = ref(response.radolanHourly)
     const timestampsDailyChart = ref(response.timestampsDaily)
     const timestampsDailyTable = ref(response.timestampsDaily)
-    const complete = ref(Math.round((response.complete * 100) / 100))
+    const complete = ref(Math.round((response.complete * 100) / 100) / 10)
+
+    async function printDate() {
+      let eventDate = new Date(eventDateString.value)
+      let startDate = new Date(eventDate)
+      startDate.setDate(eventDate.getDate() - 14)
+      let endDate = new Date(eventDate)
+      endDate.setDate(eventDate.getDate() + 14)
+
+      startDateString.value = startDate.toISOString().split("T")[0]
+      endDateString.value = endDate.toISOString().split("T")[0]
+
+      const newResponse = await getRadolanDataByFieldId(fieldId, startDateString.value, endDateString.value, true)
+
+      radolanDailyValuesArray.value = newResponse.radolanDaily
+      radolanHourlyValuesArray.value = newResponse.radolanHourly
+      timestampsDailyChart.value = newResponse.timestampsDaily
+      timestampsDailyTable.value = newResponse.timestampsDaily
+      complete.value = Math.round((newResponse.complete * 100) / 100) / 10
+
+      chartKey.value += 1
+      tableKey.value += 1
+    }
 
     return {
       fieldId,
@@ -73,7 +119,15 @@ export default defineComponent({
       timestampsDailyTable,
       radolanDailyValuesArray,
       radolanHourlyValuesArray,
-      complete
+      complete,
+      printDate,
+      startDateString,
+      endDateString,
+      startDatePrint,
+      endDatePrint,
+      eventDateString,
+      chartKey,
+      tableKey
     };
   }
 });
