0256300934692ad4a940c147682bec6b3a1fb9b4
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:18:52 +0100

    Update: user-index view to display field cards & map

--- client/src/views/user/_id/index.vue
+++ client/src/views/user/_id/index.vue
@@ -1,24 +1,79 @@
 // INDEX FILE FOR USER/:ID
 
 <template>
-  <div>
-    <h1>{{ userId }}</h1>
+  <div class="body">
+    <h1>{{ user.firstname }} {{ user.lastname }}</h1>
+    <router-link to="/drawmap">
+      <button>Add Field</button>
+    </router-link>
+    <div class="fields__container">
+      <div class="field__card" v-for="field in fields" :key="field.id">
+        <h3> {{ field.name }}</h3>
+        <router-link class="field__button" :to='{ name: "Field", params: {userId: user.id, fieldId: field.id}}'>
+          <button>Mehr anzeigen...</button>
+        </router-link>
+      </div>
+    </div>
+    <div class="map__container">
+      <fields-map :userId="user.id" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import {
+  defineComponent,
+  ref,
+  computed,
+  onMounted
+} from "vue";
+// import { useGetters, useState } from "vuex-composition-helpers";
+import { useStore } from "vuex";
 import { useRoute } from "vue-router";
+
+import { getFieldsByUser } from "../../../utils/functions/getFieldsByUser"
+import FieldsMap from "../../../components/FieldsMap.vue";
 
 export default defineComponent({
   name: "indexUser",
-  components: {},
+  components: { FieldsMap },
 
-  setup() {
+  async setup() {
     const route = useRoute();
-    const userId = ref(route.params.userId);
+    const store = useStore();
 
-    return { userId }
+    let user = computed(() => store.state.user).value
+
+    const fields = (await getFieldsByUser(user.id)).response.data
+
+    return {
+      user,
+      fields
+    }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.fields__container {
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 90vw;
+}
+.map__container {
+  position: relative;
+  height: 40vh;
+  width: 100vw;
+}
+.field__card {
+  width: 20vw;
+  height: 15vh;
+  background: lightgray;
+  border: 1px grey solid;
+  border-radius: 5px;
+  margin: 2vh;
+}
+</style>
