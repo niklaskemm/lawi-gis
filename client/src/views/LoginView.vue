311b7071dbfbf729eaffc25af2aeabdf009ccc43
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:17:24 +0100

    Add: login&register views

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <login-component />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import LoginComponent from "../components/Login.vue";
+
+export default defineComponent({
+  name: "LoginView",
+  components: {
+    LoginComponent
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <register-component />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import RegisterComponent from "../components/Register.vue";
+
+export default defineComponent({
+  name: "RegisterView",
+  components: {
+    RegisterComponent
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
