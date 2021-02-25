6037f59a0a8f3c1a1f3d9bf09073d3f6883d588e
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:18:00 +0100

    Update: remove login&register comps from home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
     <h1>This is the Home page.</h1>
-    <login />
-    <register />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import Login from "../components/Login.vue";
-import Register from "../components/Register.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    Login,
-    Register
   }
 });
 </script>
