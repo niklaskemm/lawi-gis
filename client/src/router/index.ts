22299d95f6f8e7854617dd7b95bc04db01f9045a
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:15:13 +0100

    Update: add login&register FE-routing

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,10 +1,23 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+import store from "../store"
 
-const routes: Array<RouteRecordRaw> = [
+import { getFieldById } from "../utils/functions/getFieldById"
+
+const routes = [
   {
     path: "/",
     name: "Home",
     component: () => import("../views/Home.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/LoginView.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/RegisterView.vue")
   },
   {
     path: "/about",
@@ -19,7 +32,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/drawmap",
     name: "DrawMap",
-    component: () => import("../views/DrawMapView.vue")
+    component: () => import("../views/DrawMapView.vue"),
+    meta: {
+      requiresLogin: true
+    }
   },
   {
     path: "/grid/:gridId",
@@ -29,18 +45,18 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/user/:userId",
     name: "User",
-    component: () => import("../views/user/_id/index.vue")
-  },
-
-  {
-    path: "/user/:userId/fields",
-    name: "UserFields",
-    component: () => import("../views/user/_id/fields/index.vue")
+    component: () => import("../views/user/_id/index.vue"),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
-    path: "/field/:fieldId",
+    path: "/user/:userId/:fieldId",
     name: "Field",
-    component: () => import("../views/field/_id/index.vue")
+    component: () => import("../views/field/_id/index.vue"),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: "/testing",
@@ -56,6 +72,34 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  let requiresLogin = false
+  let requiresAuth = false
+  to.matched.some((record) => {
+    if (record.meta.requiresLogin) {
+      requiresLogin = true
+    } else if (record.meta.requiresAuth) {
+      requiresAuth = true
+    }
+  })
+
+  if (requiresLogin) {
+    if (store.getters.isLoggedIn) {
+      next()
+    } else {
+      next("/login")
+    }
+  } else if (requiresAuth) {
+    if (store.getters.getUser.id != to.params.userId) {
+      next("login")
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
