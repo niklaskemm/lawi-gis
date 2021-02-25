c8ea032023676c27528def8fe2230fabeeddaaf1
Niklas Kemm <niklas.kemm@googlemail.com>
2021-02-25 11:16:06 +0100

    update: add store config for user/jwt storage

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,41 @@
-import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate"
+import { createStore } from "vuex"
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    user: null,
+    token: null
+  },
+
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload
+    },
+    setToken(state, payload) {
+      state.token = payload
+    }
+  },
+
+  actions: {
+    setUser({ commit }, payload) {
+      commit("setUser", payload)
+    },
+    setToken({ commit }, payload) {
+      commit("setToken", payload)
+    }
+  },
+
+  modules: {},
+  getters: {
+    getUser(state) {
+      return state.user
+    },
+    getToken(state) {
+      return state.token
+    },
+    isLoggedIn(state) {
+      return !!state.token
+    }
+  },
+  plugins: [createPersistedState()]
+})
