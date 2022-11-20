715e7caa3a218ed8eb85a175a6e2f9f23302b1e9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-20 13:07:06 +0000

    Adding tarfile member sanitization to extractall()

--- harvester/radolan/unpack.py
+++ harvester/radolan/unpack.py
@@ -41,7 +41,26 @@ def unpack_data():
             tarlist = []
             for member in tar.getmembers():
                 tarlist.append(member)
-            tar.extractall(temp_path_unpacked, tarlist)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, temp_path_unpacked, tarlist)
             tar.close()
 
             os.remove(dest.split(".gz")[0])
